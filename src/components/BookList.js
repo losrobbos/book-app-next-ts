f1e064af4d811de5ab254b0ca922d754a0384ea7
losrobbos <losrobbos@web.de>
2024-06-13 19:01:38 +0200

    Added booklist page

---
+++ src/components/BookList.js
@@ -0,0 +1,56 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { books as booksApi } from '@/data/db'
+
+export const BookList = () => {
+
+  // use initial API books data for initial server side rendering
+  const [books, setBooks] = useState(booksApi || []);
+
+  // get newest data on client after initial render
+  useEffect(() => {
+    fetch(`/api/books`)
+    .then(res => res.json())
+    .then(booksApi => setBooks(booksApi))
+  }, [])
+
+  // render list
+  const jsxBooks = books.map((book) => (
+    <div key={book._id} className="border-red-900 rounded-xl border-2 p-2">
+      <div>{book.title}</div>
+      <div className="text-sm">{book.author}</div>
+      <button onClick={() => deleteBook(book._id)}>X</button>
+    </div>
+  ));
+
+  const addBook = () => {
+    const bookNew = {
+      title: "Random Title" + Date.now(),
+      author: "Randy" + Date.now(),
+    };
+
+    fetch("/api/books", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(bookNew),
+    })
+      .then((res) => res.json())
+      .then((bookNewApi) => {
+        console.log({ bookNewApi });
+        setBooks([...books, bookNewApi]);
+      });
+  };
+
+  const deleteBook = (bookId) => {
+    setBooks(books.filter((book) => book._id !== bookId));
+  };
+
+  return (
+    <div>
+      <h1 className="text-2xl">Book List</h1>
+      <div className="py-4 flex gap-3 flex-wrap">{jsxBooks}</div>
+      <button onClick={addBook}>Add</button>
+    </div>
+  );
+};
---
+++ src/data/db.js
@@ -0,0 +1,4 @@
+export const books = [
+  { _id: "b1", title: "Name of the Wind", author: "Brandon Sanderson" },
+  { _id: "b2", title: "Name of the Rose", author: "Umberto Eco" },
+];
